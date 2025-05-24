4c011881a59d22b33e6a1c2fe3f113ea7da4f4a1
Alex-Winterfish <Alksbulgakov@gmail.com>
2025-05-24 08:50:38 +0300

    изменен settings.py

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["158.160.182.84"]
 
 # Application definition
 
