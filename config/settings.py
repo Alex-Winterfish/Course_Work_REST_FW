f4d851ac15ff036588297cde389c9357d125f46b
Alex-Winterfish <Alksbulgakov@gmail.com>
2025-05-25 20:21:36 +0300

    в переменые окружения добавлен IP хоста, изменен docker-compose.yml: статические файли собираются в Dockerfile приложения.

--- config/settings.py
+++ config/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["158.160.182.84", "158.160.176.75"]
+ALLOWED_HOSTS = [os.getenv('AllOWED_HOSTS')]
 
 # Application definition
 
